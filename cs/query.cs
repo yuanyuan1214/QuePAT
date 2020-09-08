b7357e2188d7dbaff43c4d968eb508bec0f0425b
Yan-1999 <yan19991218@163.com>
2020-09-08 19:28:31 +0800

    implement connection

---
+++ cs/query.cs
@@ -0,0 +1,42 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+
+namespace QuePAT
+{
+    partial class QuePATDataBase
+    {
+        public void Query()
+        {
+            try
+            {
+                string querySqlStr = "SELECT * FROM province";
+                Console.WriteLine(querySqlStr);
+                try
+                {
+                    OracleCommand cmd = new OracleCommand(querySqlStr, conn);
+                    Console.WriteLine("code\tname");
+                    using (var dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            Console.WriteLine
+                                (
+                                    "{0}\t{1}",
+                                    dr.GetValue(0).ToString(),
+                                    dr.GetValue(1).ToString()
+                                );
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
